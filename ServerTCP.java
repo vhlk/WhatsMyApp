fe316c69d3275f3f203634585e1255388008760e
vhlk <38112094+vhlk@users.noreply.github.com>
2019-06-18 10:11:20 -0300

    Add files via upload

---
+++ ServerTCP.java
@@ -0,0 +1,64 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerTCP {
+
+	public static void main(String[] args) {
+		int port = 8888;
+        try {
+            ServerSocket tmpSocketIn = new ServerSocket(port);
+
+            ServerSocket tmpSocketOut = new ServerSocket(8421);
+            System.out.println("Aguardando cliente");
+            Socket socketIn = tmpSocketIn.accept();
+            Socket socketOut = tmpSocketOut.accept();
+            DataInputStream inputStream;
+            DataOutputStream outputStream;
+            String strData, ans;
+            
+            while (true) {
+            	inputStream = new DataInputStream(socketIn.getInputStream());
+            	strData = inputStream.readUTF();
+            	strData = strData.trim();
+                System.out.println("Texto recebido do cliente: " + strData);
+                
+                if (strData.contains("InfraCom")) {
+                    ans = "IF678, a disciplina de InfraEstrutura de Comunicacao ministrada pelo professor Paulo Goncalves ";
+                } else if (strData.contains("Ola") || strData.contains("Oi")) {
+                    ans = "Ola, como esta voce?";
+                } else if (strData.contains("Tudo bem?")) {
+                    ans = "Tudo, e com voce?";
+                } else if (strData.contains("Adriano")) {
+                    ans = "O melhor monitor!";
+                } else {
+                    ans = "Mensagem recebida!";
+                }
+                outputStream = new DataOutputStream(socketOut.getOutputStream());
+                outputStream.writeUTF(ans);
+                System.out.println("Enviado");
+            }
+            
+
+        } catch (BindException e) {
+            System.out.println("Endereco em uso");
+        } catch (Exception e) {
+            System.out.println("Erro" + e);
+        }
+
+	}
+
+}
