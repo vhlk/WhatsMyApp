a1e930f583ab16fb56cd4ff7905c7cf3439b34ed
vhlk <38112094+vhlk@users.noreply.github.com>
2019-06-24 19:10:21 -0300

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,51 +1,55 @@
-import java.io.IOException;
+package teste;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.net.*;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
 	public static void main(String[] args) {
 		int port = 8888;
-        try {
-            ServerSocket tmpSocketIn = new ServerSocket(port);
+		try {
+			ServerSocket tmpSocketIn = new ServerSocket(port);
+			ServerSocket tmpSocketOut = new ServerSocket(8421);
+			System.out.println("Aguardando cliente");
+			Socket socketIn = tmpSocketIn.accept();
+			Socket socketOut = tmpSocketOut.accept();
+			DataInputStream inputStream;
+			DataOutputStream outputStream;
+			String strData, ans;
 
-            ServerSocket tmpSocketOut = new ServerSocket(8421);
-            System.out.println("Aguardando cliente");
-            Socket socketIn = tmpSocketIn.accept();
-            Socket socketOut = tmpSocketOut.accept();
-            DataInputStream inputStream;
-            DataOutputStream outputStream;
-            String strData, ans;
-            
-            while (true) {
-            	inputStream = new DataInputStream(socketIn.getInputStream());
-            	strData = inputStream.readUTF();
-            	strData = strData.trim();
-                System.out.println("Texto recebido do cliente: " + strData);
-                
-                if (strData.contains("InfraCom")) {
-                    ans = "IF678, a disciplina de InfraEstrutura de Comunicacao ministrada pelo professor Paulo Goncalves ";
-                } else if (strData.contains("Ola") || strData.contains("Oi")) {
-                    ans = "Ola, como esta voce?";
-                } else if (strData.contains("Tudo bem?")) {
-                    ans = "Tudo, e com voce?";
-                } else if (strData.contains("Adriano")) {
-                    ans = "O melhor monitor!";
-                } else {
-                    ans = "Mensagem recebida!";
-                }
-                outputStream = new DataOutputStream(socketOut.getOutputStream());
-                outputStream.writeUTF(ans);
-                System.out.println("Enviado");
-            }
-            
+			while (true) {
+				inputStream = new DataInputStream(socketIn.getInputStream());
+				strData = inputStream.readUTF();
+				strData = strData.trim();
+				System.out.println("Texto recebido do cliente: " + strData);
+				if (strData.charAt(0) == 1) {
+					ans = "ack";
+				} else if (strData.charAt(0) == 2) {
+					ans = "apagar" + strData.substring(1, strData.length());
+				} else if (strData.contains("InfraCom")) {
+					ans = "IF678, a disciplina de InfraEstrutura de Comunicacao ministrada pelo professor Paulo Goncalves ";
+				} else if (strData.contains("Ola") || strData.contains("Oi")) {
+					ans = "Ola, como esta voce?";
+				} else if (strData.contains("Tudo bem?")) {
+					ans = "Tudo, e com voce?";
+				} else if (strData.contains("Adriano")) {
+					ans = "O melhor monitor!";
+				} else {
+					ans = "Mensagem recebida!";
+				}
+				outputStream = new DataOutputStream(socketOut.getOutputStream());
+				outputStream.writeUTF(ans);
+				System.out.println("Enviado");
+			}
 
-        } catch (BindException e) {
-            System.out.println("Endereco em uso");
-        } catch (Exception e) {
-            System.out.println("Erro" + e);
-        }
+		} catch (BindException e) {
+			System.out.println("Endereco em uso");
+		} catch (Exception e) {
+			System.out.println("Erro" + e);
+		}
 
 	}
 
---
+++ Teste.java
@@ -0,0 +1,120 @@
+package teste;
+
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Teste {
+
+	public static void main(String args[]) throws IOException {
+		int port = 8888;// Porta do servidor
+		String address = "localhost";// host do servidor
+
+		try {
+			Socket socket = new Socket(address, port);
+			char flag = '0';
+			Scanner in = new Scanner(System.in);
+			String clientMsg;
+			DataOutputStream outputStream;
+			Mensagens mensagens = new Mensagens();
+			receiveThread rt = new receiveThread();
+			rt.start();
+
+			while (true) {
+				clientMsg = in.nextLine();
+				if (clientMsg.equals("quero apagar")) {
+					flag = '2';
+					int posicao = in.nextInt();
+				}
+				outputStream = new DataOutputStream(socket.getOutputStream());
+				outputStream.writeUTF(flag + clientMsg);
+				outputStream.flush();
+				for (int i = 0; i < mensagens.mensagens.size(); i++) {
+					System.out.println(mensagens.mensagens.get(i));
+				}
+				flag = '0';
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class receiveThread extends Thread {
+
+	public receiveThread() {
+	}
+
+	public void run() {
+
+		try {
+			String serverMsg;
+			int port = 8421;// Porta do cliente
+			String address = "localhost";// host do servidor
+
+			DataInputStream inputStream;
+			Socket socket = new Socket(address, port);
+
+			while (true) {
+				inputStream = new DataInputStream(socket.getInputStream());
+				serverMsg = inputStream.readUTF();
+				if (serverMsg.contains("apagar")) {
+					serverMsg.trim();
+					Mensagens.mensagens.remove(parseInteger(serverMsg.substring(6, serverMsg.length())));
+				}
+				else {
+					Mensagens.mensagens.addElement(serverMsg);
+					System.out.println(serverMsg);
+					ackThread at = new ackThread();
+					at.start();
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private Object parseInteger(String substring) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+class ackThread extends Thread {
+
+	public ackThread() {
+	}
+
+	public void run() {
+
+		try {
+			DataOutputStream outputStream;
+			int port = 8888;// Porta do servidor
+			String address = "localhost";// host do servidor
+
+			Socket socket = new Socket(address, port);
+
+			outputStream = new DataOutputStream(socket.getOutputStream());
+			outputStream.writeUTF("1");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class Mensagens {
+	public static Vector<String> mensagens;
+
+	public Mensagens() {
+		this.mensagens = new Vector<String>();
+	}
+}
