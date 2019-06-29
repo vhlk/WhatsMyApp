67b6c61a0c2276ec95fbda4cad7e7e63c79d8abd
vhlk <vhlk@cin.ufpe.br>
2019-06-29 03:33:09 -0300

    Para evitar complicações, foi deixado apenas o servidor e o cliente. Infelizmente o cliente não consegue receber mensagens que deveriam ser enviadas pelo servidor

---
+++ Cliente1.java
@@ -0,0 +1,155 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
+import java.util.Scanner;
+import java.util.Vector;
+
+/* WhatsMyApp é um aplicativo simples de envio de mensagens que utilizada protocolo TCP e aprensenta as seguintes funcionalidades:
+ *  -Marcação de mensagem recebida pelo servidor
+ *  -Marcação de mensagem recebida no destinatário
+ *  -Marcação de mensagem lida
+ *  -Opção de apagar mensagem (para todos)
+ *  
+ * Nesta versão inicial será realizado para funcionar apenas para dois clientes (utilizando um servidor central) 
+ *  
+ *  Trabalho realizado por:
+ *  	Anderson Cesar de Carvalho Silva
+ *		Johnny Herbert Muniz Nunes
+ *		Jose Guilherme Nascimento Vieira da Silva
+ *		Luana Porciuncula Barreto
+ *		Samuel Oliveira de Miranda
+ *		Victor Hugo de Lima Kunst
+ */
+
+
+public class Cliente1 {
+	
+	/*
+	 * Flags utilizadas:
+	 * 		1: confirmação de recebimento pelo cliente
+	 * 		2: mensagem de pedido de apagar mensagem
+	 * 		3: apagar mensagem (seguido pela posicao a ser apagada)
+	 */
+
+	public static void main(String args[]) throws IOException {
+		int port = 8888;// Porta do servidor
+		String address = "localhost";// host do servidor
+
+		try {
+			Socket socket = new Socket(address, port);
+			char flag = '0';
+			Scanner in = new Scanner(System.in);
+			String clientMsg;
+			//DataOutputStream outputStream;
+			Mensagens mensagens = new Mensagens();
+			ReceiveThread rt = new ReceiveThread();
+			rt.start();
+
+			while (true) {
+				clientMsg = in.nextLine();
+				if (clientMsg.equals("quero apagar")) {
+					System.out.println("Digite a posição da mensagem a ser apagada, por favor:");
+					flag = '2'; //flag para apagar
+					int posicao = in.nextInt();
+					clientMsg = Integer.toString(posicao);
+				}
+				/*
+				outputStream = new DataOutputStream(socket.getOutputStream());
+				outputStream.writeUTF(flag + clientMsg);
+				outputStream.flush();
+				*/
+				PrintStream saida = new PrintStream(socket.getOutputStream());
+				saida.println(flag + clientMsg);
+				/* imprimir todas as mensagens do cliente
+				for (int i = 0; i < mensagens.mensagens.size(); i++) {
+					System.out.println(mensagens.mensagens.get(i));
+				} */
+				flag = '0';
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class ReceiveThread extends Thread {
+
+	public ReceiveThread() {
+	}
+
+	public void run() {
+
+		try {
+			String serverMsg;
+			int port = 8421;// Porta do cliente
+			String address = "localhost";// host do servidor
+
+			BufferedReader input;
+			//DataInputStream inputStream;
+			Socket socket = new Socket(address, port);
+
+			while (true) {
+				input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				System.out.println("**");
+				serverMsg = input.readLine();
+				System.out.println("*"); //coloquei esses prints para mostrar que ele nao recebe mensagem do server
+				if (serverMsg.charAt(0) == '3') {
+					for (int i=0;i<50;i++) System.out.println();
+					Mensagens.mensagens.remove(Integer.parseInt(serverMsg.substring(1, serverMsg.length())));
+					System.out.println(Integer.parseInt(serverMsg.substring(1, serverMsg.length())));
+					for (int i=0;i<Mensagens.mensagens.size();i++) {
+						System.out.println(Mensagens.mensagens.elementAt(i));
+					}
+				}
+				else {
+					Mensagens.mensagens.addElement(serverMsg);
+					System.out.println(serverMsg);
+					ACKMensagemRecebida at = new ACKMensagemRecebida();
+					at.start();
+				}
+				System.out.println("***");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class ACKMensagemRecebida extends Thread {
+
+	public ACKMensagemRecebida() {}
+
+	public void run() {
+
+		try {
+			//DataOutputStream outputStream;
+			int port = 8888;// Porta do servidor
+			String address = "localhost";// host do servidor
+
+			Socket socket = new Socket(address, port);
+
+			PrintStream saida = new PrintStream(socket.getOutputStream());
+			saida.println("1");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class Mensagens {
+	public static Vector<String> mensagens;
+
+	public Mensagens() {
+		this.mensagens = new Vector<String>();
+	}
+}
--- Server.java
+++ Server.java
@@ -1,27 +1,40 @@
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
 public class Server {
-
+	public static Vector<String> mensagens = new Vector<String>();
+	private Socket conexao;
+	private static Vector CLIENTES;
+	
+	public Server(Socket socket)
+	{
+		this.conexao = socket;
+	}
+	
 	public static void main(String[] args) {
 		int port = 8888;
 		try {
 			ServerSocket tmpSocketIn = new ServerSocket(port);
 			ServerSocket tmpSocketOut = new ServerSocket(8421);
-			System.out.println("Aguardando cliente");
-			Socket socketIn = tmpSocketIn.accept();
-			Socket socketOut = tmpSocketOut.accept();
-			DataInputStream inputStream;
-			DataOutputStream outputStream;
-			String strData, ans;
-
+			
 			while (true) {
-				inputStream = new DataInputStream(socketIn.getInputStream());
+				System.out.println("Aguardando cliente");
+				Socket socketIn = tmpSocketIn.accept();
+				Socket socketOut = tmpSocketOut.accept();
+				DataInputStream inputStream;
+				DataOutputStream outputStream;
+				String strData, ans;
+				/*inputStream = new DataInputStream(socketIn.getInputStream());
 				strData = inputStream.readUTF();
 				strData = strData.trim();
+				
 				System.out.println("Texto recebido do cliente: " + strData);
 				if (strData.charAt(0) == 1) { //1 -> ack p/ cliente
 					ans = "ack";
@@ -30,7 +43,7 @@ public class Server {
 				} else if (strData.contains("InfraCom")) {
 					ans = "IF678, a disciplina de InfraEstrutura de Comunicacao ministrada pelo professor Paulo Goncalves ";
 				} else if (strData.contains("Ola") || strData.contains("Oi")) {
-					ans = "Ola, como esta voce?";
+					ans = "Ola, como esta voce?s";
 				} else if (strData.contains("Tudo bem?")) {
 					ans = "Tudo, e com voce?";
 				} else if (strData.contains("Adriano")) {
@@ -38,9 +51,14 @@ public class Server {
 				} else {
 					ans = "Mensagem recebida!";
 				}
-				outputStream = new DataOutputStream(socketOut.getOutputStream());
-				outputStream.writeUTF(ans);
+				*/
+				ReceiveMsg t = new ReceiveMsg(socketIn);
+				t.start();
+				PrintStream saida = new PrintStream(socketOut.getOutputStream());
+				//outputStream = new DataOutputStream(socketOut.getOutputStream());
+				saida.println("Recebi");
 				System.out.println("Enviado");
+				
 			}
 
 		} catch (BindException e) {
@@ -50,5 +68,108 @@ public class Server {
 		}
 
 	}
+	
+	/*public void run(Socket socketClient)
+	{
+		try
+		{
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+			PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+			saida.println("Conectado");
+			mensagens.add(entrada.readLine());
+			System.out.println(entrada.readLine());
+			System.out.println(mensagens.get(0));
+			
+			
+			
+			DataInputStream inputStream;
+			String strData;
+			inputStream = new DataInputStream(socketClient.getInputStream());
+			strData = inputStream.readUTF();
+			strData = strData.trim();
+			mensagens.add(strData);
+			System.out.println(mensagens.get(0));
+			System.out.println(strData);
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Deu erro!");
+		}
+	}*/
 
 }
+
+class ReceiveMsg extends Thread
+{
+	public static Vector<String> mensagens = new Vector<String>();
+	public static Vector<String> usuarios = new Vector<String>();
+	
+	private Socket conexao;
+	
+	public ReceiveMsg(Socket conexao)
+	{
+		this.conexao = conexao;
+	}
+	
+	public void run()
+	{
+		try
+		{
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+			PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+
+			saida.println("Conectado");
+			String nome = entrada.readLine();
+			usuarios.add(nome);
+			String mensagem = "";
+			mensagem = entrada.readLine();
+			int cont = 0;
+			while(mensagem != null)
+			{
+				ackThreadS ack = new ackThreadS();
+				ack.start();
+				if (mensagem.charAt(0) == '3') {
+					mensagens.remove(Integer.parseInt(mensagem.substring(1, mensagem.length())));
+					cont--;
+				}
+				else {
+					mensagens.add(mensagem);
+					System.out.println(mensagens.get(cont));
+				}
+				mensagem = entrada.readLine();
+				cont++;
+			}
+			System.out.println("Sai da conversa");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Deu erro!");
+		}
+	}
+
+}
+class ackThreadS extends Thread {
+
+	public ackThreadS() {
+	}
+
+	public void run() {
+
+		try {
+			DataOutputStream outputStream;
+			int port = 8888;// Porta do servidor
+			String address = "localhost";// host do servidor
+
+			Socket socket = new Socket(address, port);
+
+			outputStream = new DataOutputStream(socket.getOutputStream());
+			outputStream.writeUTF("1");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+	
